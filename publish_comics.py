d91e1139648309ac58b6c1b0f2e37bc97df77f44
DarinaAnikeeva <anikeevadarina29@gmail.com>
2022-07-14 14:17:53 +0300

    Update publish_comics.py

--- publish_comics.py
+++ publish_comics.py
@@ -7,12 +7,13 @@ from comics_helper import save_photos
 
 
 def send_comic_to_server(access_token, group_id, picture):
+    send_to_server_url = "https://api.vk.com/method/photos.getWallUploadServer"
     payload = {
         'access_token': access_token,
         'group_id': group_id,
         "v": LAST_API_VERSION
     }
-    response = requests.get("https://api.vk.com/method/photos.getWallUploadServer", params=payload)
+    response = requests.get(send_to_server_url, params=payload)
     response.raise_for_status()
     upload_url = response.json()['response']['upload_url']
     with open(picture, 'rb') as file:
@@ -27,6 +28,7 @@ def send_comic_to_server(access_token, group_id, picture):
 
 def save_comic_to_album(access_token, group_id, picture):
     server, photo, hash = send_comic_to_server(access_token, group_id, picture)
+    save_comic_url = "https://api.vk.com/method/photos.saveWallPhoto"
     album_picture_payload = {
         "server": server,
         'photo': photo,
@@ -35,7 +37,7 @@ def save_comic_to_album(access_token, group_id, picture):
         'access_token': access_token,
         "v": LAST_API_VERSION,
     }
-    album_picture_response = requests.post("https://api.vk.com/method/photos.saveWallPhoto", params=album_picture_payload)
+    album_picture_response = requests.post(save_comic_url, params=album_picture_payload)
     album_picture_response.raise_for_status()
     picture_id = album_picture_response.json()['response'][0]['id']
     picture_owner_id = album_picture_response.json()['response'][0]['owner_id']
@@ -44,6 +46,7 @@ def save_comic_to_album(access_token, group_id, picture):
 
 def send_comic_to_wall(access_token, group_id, message, picture):
     picture_media_id, picture_owner_id = save_comic_to_album(access_token, group_id, picture)
+    wall_post_url = "https://api.vk.com/method/wall.post"
     wall_picture_payload = {
         'from_group': 1,                                                  # 1 - сообщение от имени группы, 0 - от имени пользователя
         'attachments': f"photo{picture_owner_id}_{picture_media_id}",     #для поиска нужной картинки
@@ -52,7 +55,7 @@ def send_comic_to_wall(access_token, group_id, message, picture):
         'access_token': access_token,
         'v': LAST_API_VERSION
     }
-    wall_picture_response = requests.post("https://api.vk.com/method/wall.post", params=wall_picture_payload)
+    wall_picture_response = requests.post(wall_post_url, params=wall_picture_payload)
     wall_picture_response.raise_for_status()
 
 
